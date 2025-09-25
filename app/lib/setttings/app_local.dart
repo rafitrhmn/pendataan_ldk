3db0614c08840e7a0215a1d53db44d31fb5a8416
rafitrhmn <rafitaufih10@gmail.com>
2025-09-25 15:16:21 +0700

    app_local utk default bahasa indonesia

---
+++ app/lib/setttings/app_local.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_localization/flutter_localization.dart';
+
+mixin AppLocale {
+  static const String title = 'title';
+
+  static const Map<String, dynamic> EN = {title: 'Localization'};
+
+  static const Map<String, dynamic> ID = {title: 'Lokalisasi'};
+}
